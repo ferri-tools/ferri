c45985493aa0a1ec16a4703b247fd8c1fc06cac7
Jorge A. Jimenez <me@jorgeajimenez.com>
2025-10-12 13:34:35 -0700

    feat(engine): #48 Define the core Executor trait

--- crates/ferri-automation/src/lib.rs
+++ crates/ferri-automation/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod execute;
 pub mod expressions;
+pub mod executors;
 pub mod flow;
 pub mod jobs;
+pub mod models;
 pub mod orchestrator;
---
+++ crates/ferri-automation/src/models.rs
@@ -0,0 +1,3 @@
+pub struct Workload {
+    pub command: String,
+}
